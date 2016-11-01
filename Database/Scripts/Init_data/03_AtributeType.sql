f78c9b949069e06745ed3a727753e9afad262a56
NastyaViter <nastya.bodrova@gmail.com>
2016-11-01 22:42:46 +0200

    Инициализация AtributeType and EntityType

---
+++ Database/Scripts/Init_data/03_AtributeType.sql
@@ -0,0 +1,43 @@
+delete from  ns_admin.TBL_ATRIBUTETYPE;
+commit;
+INSERT INTO ns_admin.TBL_ATRIBUTETYPE (
+  ATRIBUTETYPEID,
+	ATRIBUTETYPENAME) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'String');
+  
+  INSERT INTO ns_admin.TBL_ATRIBUTETYPE (
+  ATRIBUTETYPEID,
+	ATRIBUTETYPENAME) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Integer');
+  
+  INSERT INTO ns_admin.TBL_ATRIBUTETYPE (
+  ATRIBUTETYPEID,
+	ATRIBUTETYPENAME) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Decimal');
+  
+  INSERT INTO ns_admin.TBL_ATRIBUTETYPE (
+  ATRIBUTETYPEID,
+	ATRIBUTETYPENAME) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Date');
+  
+  INSERT INTO ns_admin.TBL_ATRIBUTETYPE (
+  ATRIBUTETYPEID,
+	ATRIBUTETYPENAME) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Boolean');
+  
+  commit;
---
+++ Database/Scripts/Init_data/04_EntityType.sql
@@ -0,0 +1,43 @@
+delete from  ns_admin.TBL_ENTITYTYPE;
+commit;
+INSERT INTO ns_admin.TBL_ENTITYTYPE (
+  ENTITYTYPEID,
+	ENTITYTYPENAME,
+  ISACTIVE) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Order',
+  1);
+  
+  INSERT INTO ns_admin.TBL_ENTITYTYPE (
+  ENTITYTYPEID,
+	ENTITYTYPENAME,
+  ISACTIVE) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Product',
+  1);
+  
+  INSERT INTO ns_admin.TBL_ENTITYTYPE (
+  ENTITYTYPEID,
+	ENTITYTYPENAME,
+  ISACTIVE) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Telephone',
+  1);
+  
+  INSERT INTO ns_admin.TBL_ENTITYTYPE (
+  ENTITYTYPEID,
+	ENTITYTYPENAME,
+  ISACTIVE) 
+	values
+	(
+	SQ_MAIN.nextval,
+	'Tablet',
+  1);
+  
+  commit;
