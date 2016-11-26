d1e2803c9f42a97920a69aa9221486396a888248
DiRaskolnikova <diana377577@gmail.com>
2016-11-26 15:54:34 +0300

    product extends entity

--- src/main/java/com/netcracker/crm/entity/serviceEntity/Product.java
+++ src/main/java/com/netcracker/crm/entity/serviceEntity/Product.java
@@ -1,46 +1,42 @@
 package com.netcracker.crm.entity.serviceEntity;
 
 
-import com.netcracker.crm.entity.AbstractEntity;
-import com.netcracker.crm.entity.Atribute;
 import com.netcracker.crm.entity.Entity;
 import com.netcracker.crm.entity.Value;
-import javafx.util.Pair;
 
-import java.util.ArrayList;
 import java.util.List;
 
-//возможно наследовать от entity
-public class Product extends AbstractEntity {
-    private String name;    //название телефона или планшета
-    private boolean isActive;
-    private String productType; //телефон или планшет
-    private int userId;
+public class Product extends Entity {
+//    private String name;    //название телефона или планшета
+//    private boolean isActive;
+//    private String productType; //телефон или планшет
+//    private int userId;
     private double price;
     private String summary;
     private int orderId; //0 - не в заказе
     private String imageUrl;
     private int quantity;
-    private List<Pair<Atribute, Value>> atributeValueMap;  //атрибуты телефона (камера и т.п.)
+//    private List<Pair<Atribute, Value>> atributeValueMap;  //атрибуты телефона (камера и т.п.)
 
 
     public Product() {
     }
 
-    public Entity toEntity() {
-        Entity entity = new Entity(getId(), name, isActive,
-                EntityType.valueOf(productType).getTypeId(), productType, userId);
-        entity.setAtributeValueMap(atributeValueMap);
-        return entity;
-    }
+//    public Entity toEntity() {
+//        Entity entity = new Entity(getId(), name, isActive,
+//                EntityType.valueOf(productType).getTypeId(), productType, userId);
+//        entity.setAtributeValueMap(atributeValueMap);
+//        return entity;
+//    }
 
     public Product(Entity entity) {
-        this(entity.getId(), entity.getEntityName(), entity.getisActive() == 1 ? true : false,
-                entity.getEntityTypeName(), entity.getEntityUserId(), entity.getAtributeValueMap());
-        if (atributeValueMap != null) {
-            for (int i = 0; i < atributeValueMap.size(); i++) {
-                String atributeName = atributeValueMap.get(i).getKey().getAtributeName();
-                String atributeValue = atributeValueMap.get(i).getValue().getValue();
+        super(entity.getId(), entity.getEntityName(), entity.getisActive() == 1 ? true : false,
+                entity.getEntityTypeId(), entity.getEntityTypeName(), entity.getEntityUserId());
+        setAtributeValueMap(entity.getAtributeValueMap());
+        if (getAtributeValueMap() != null) {
+            for (int i = 0; i < getAtributeValueMap().size(); i++) {
+                String atributeName = getAtributeValueMap().get(i).getKey().getAtributeName();
+                String atributeValue = getAtributeValueMap().get(i).getValue().getValue();
                 switch (atributeName) {
                     case "Price":
                         price = Double.parseDouble(atributeValue);
@@ -64,33 +60,25 @@ public class Product extends AbstractEntity {
         }
     }
 
-    //поля из entity и атрибуты
-    public Product(int id, String name, boolean isActive, String productType, int userId, List<Pair<Atribute, Value>> avMap) {
-        super(id);
-        this.name = name;
-        this.isActive = isActive;
-        this.productType = productType;
-        this.userId = userId;
-        this.atributeValueMap = avMap;
+    public Product(int id, String name, boolean isActive,
+                   int productTypeId, String productTypeName, int userId) {
+        super(id, name, isActive, productTypeId, productTypeName, userId);
     }
 
-    public Product(int id, String name, boolean isActive, String productType, int userId, int price, String summary) {
-        super(id);
-        this.name = name;
-        this.price = price;
-        this.productType = productType;
-        this.isActive = isActive;
-        this.userId = userId;
-        this.summary = summary;
+    public Product(String name, boolean isActive, int productTypeId, int userId, List<Value> valueList) {
+        super(name, isActive, productTypeId, userId, valueList);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public Product(int id, String name, boolean isActive, String productType, int userId, int price, String summary) {
+//        super(id);
+//        this.name = name;
+//        this.price = price;
+//        this.productType = productType;
+//        this.isActive = isActive;
+//        this.userId = userId;
+//        this.summary = summary;
+//    }
 
-    public String getName() {
-        return name;
-    }
 
     public double getPrice() {
         return price;
@@ -98,38 +86,6 @@ public class Product extends AbstractEntity {
 
     public void setPrice(double price) {
         this.price = price;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
-    public List<Pair<Atribute, Value>> getAtributeValueMap() {
-        return atributeValueMap;
-    }
-
-    public void setAtributeValueMap(List<Pair<Atribute, Value>> atributeValueMap) {
-        this.atributeValueMap = atributeValueMap;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setIsActive(boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
     }
 
     public String getSummary() {
@@ -164,15 +120,12 @@ public class Product extends AbstractEntity {
 
         Product product = (Product) o;
 
-        if (isActive() != product.isActive()) return false;
-        if (getUserId() != product.getUserId()) return false;
-        if (getPrice() != product.getPrice()) return false;
+        if (Double.compare(product.getPrice(), getPrice()) != 0) return false;
         if (getOrderId() != product.getOrderId()) return false;
-        if (!getName().equals(product.getName())) return false;
-        if (!getProductType().equals(product.getProductType())) return false;
+        if (quantity != product.quantity) return false;
         if (getSummary() != null ? !getSummary().equals(product.getSummary()) : product.getSummary() != null)
             return false;
-        return !(getAtributeValueMap() != null ? !getAtributeValueMap().equals(product.getAtributeValueMap()) : product.getAtributeValueMap() != null);
+        return !(getImageUrl() != null ? !getImageUrl().equals(product.getImageUrl()) : product.getImageUrl() != null);
 
     }
 
@@ -180,15 +133,12 @@ public class Product extends AbstractEntity {
     public int hashCode() {
         int result = super.hashCode();
         long temp;
-        result = 31 * result + getName().hashCode();
-        result = 31 * result + (isActive() ? 1 : 0);
-        result = 31 * result + getProductType().hashCode();
-        result = 31 * result + getUserId();
         temp = Double.doubleToLongBits(getPrice());
         result = 31 * result + (int) (temp ^ (temp >>> 32));
         result = 31 * result + (getSummary() != null ? getSummary().hashCode() : 0);
         result = 31 * result + getOrderId();
-        result = 31 * result + (getAtributeValueMap() != null ? getAtributeValueMap().hashCode() : 0);
+        result = 31 * result + (getImageUrl() != null ? getImageUrl().hashCode() : 0);
+        result = 31 * result + quantity;
         return result;
     }
 
@@ -196,17 +146,8 @@ public class Product extends AbstractEntity {
     public String toString() {
         return "Product{" +
                 "id='" + getId() + '\'' +
-                ", name='" + name + '\'' +
+                ", name='" + getEntityName() + '\'' +
                 ", price='" + price + '\'' +
-                ", productType='" + productType + '\'' + "}";
-    }
-
-    public List<Value> getValueList() {
-        List<Value> values = new ArrayList<>();
-        for (Pair<Atribute, Value> p : atributeValueMap) {
-            values.add(p.getValue());
-        }
-        return values;
-        //return valueList;
+                ", productType='" + getEntityTypeName() + '\'' + "}";
     }
 }
--- src/main/java/com/netcracker/crm/services/impl/ProductServiceImpl.java
+++ src/main/java/com/netcracker/crm/services/impl/ProductServiceImpl.java
@@ -2,23 +2,20 @@ package com.netcracker.crm.services.impl;
 
 import com.netcracker.crm.dao.IEntityDao;
 import com.netcracker.crm.dao.exception.DaoException;
-import com.netcracker.crm.entity.AbstractEntity;
 import com.netcracker.crm.entity.Entity;
 import com.netcracker.crm.entity.Value;
 import com.netcracker.crm.entity.serviceEntity.Product;
 import com.netcracker.crm.services.AbstractService;
-import com.netcracker.crm.services.IEntityService;
 import com.netcracker.crm.services.IProductService;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import com.netcracker.crm.services.constants.ServiceConstants;
 import com.netcracker.crm.services.exception.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by .. on 20.11.2016.
@@ -35,10 +32,9 @@ public class ProductServiceImpl  extends AbstractService<Product> implements IPr
     @Override
     public int add(Product product) {
 
-        entityDao.getByUserAndType(null, null);
         int id;
         try {
-            id = entityDao.add(product.toEntity());
+            id = entityDao.add(product);
             logger.info(ServiceConstants.TRANSACTION_SUCCEEDED + " add " + product.toString());
         }
         catch(DaoException exc){
@@ -118,7 +114,7 @@ public class ProductServiceImpl  extends AbstractService<Product> implements IPr
     @Override
     public void updateByProduct(Product product){
         try {
-            entityDao.updateByEntity(product.toEntity());
+            entityDao.updateByEntity(product);
             logger.info(ServiceConstants.TRANSACTION_SUCCEEDED + " update product #" + product.getId());
 
         } catch(DaoException exc){
