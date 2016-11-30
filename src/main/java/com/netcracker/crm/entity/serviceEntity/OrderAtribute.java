d26401141f89726f89ee36e678d029fb3cb4cebd
DiRaskolnikova <diana377577@gmail.com>
2016-12-01 01:17:57 +0300

    enum for order

---
+++ src/main/java/com/netcracker/crm/entity/serviceEntity/OrderAtribute.java
@@ -0,0 +1,27 @@
+package com.netcracker.crm.entity.serviceEntity;
+
+/**
+ * Created by οκ on 30.11.2016.
+ */
+
+public enum OrderAtribute {
+
+    OrderNumber(11),
+    ContactName(12),
+    ContactPhone(13),
+    ContactAdress(14),
+    Total(15),
+    CreatedDate(16),
+    PaidDate(17),
+    Description(18);
+
+    private int atributeId;
+
+    OrderAtribute(int id) {
+        atributeId = id;
+    }
+
+    public int getAtributeId() {
+        return atributeId;
+    }
+}
---
+++ src/main/java/com/netcracker/crm/services/parser/OrderTag.java
@@ -0,0 +1,16 @@
+package com.netcracker.crm.services.parser;
+
+import com.google.common.collect.HashBiMap;
+
+import java.util.HashMap;
+
+/**
+ * Created by οκ on 30.11.2016.
+ */
+public class OrderTag extends AbstractTag{
+    private OrderTag() {
+        tags = HashBiMap.create(new HashMap() {{
+            put("orderNumber", 11);
+        }});
+    }
+}
