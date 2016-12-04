312eef63a430d24f39520f3c69582b5650d3293d
NastyaViter <nastya.bodrova@gmail.com>
2016-12-04 16:25:46 +0300

    Add files via upload

--- src/main/java/com/netcracker/crm/dao/validation/EntityDaoValidation.java
+++ src/main/java/com/netcracker/crm/dao/validation/EntityDaoValidation.java
@@ -1,15 +1,21 @@
 package com.netcracker.crm.dao.validation;
 
 import com.netcracker.crm.dao.IEntityDao;
+import static com.netcracker.crm.dao.constants.DaoConstants.*;
+
+import com.netcracker.crm.dao.exception.DaoException;
 import com.netcracker.crm.entity.Entity;
 import com.netcracker.crm.entity.Value;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by Nastya on 12/4/2016.
  */
 public class EntityDaoValidation implements IEntityValidation  {
+    private final List<String> operators = (Arrays.asList(">","<","<=",">=","=","like","isnull"));
 
     @Override
     public void updateValidation(int id, String entityName, int isActive, int userId, List<Value> valuesArr) {
@@ -22,17 +28,26 @@ public class EntityDaoValidation implements IEntityValidation  {
     }
 
     @Override
+    public void getByIdValidation(int id) {
+
+    }
+
+    @Override
     public void updateByEntityValidation(Entity entity) {
 
     }
 
     @Override
     public void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView, int pageNumber, int pageSize) {
-
+        if(pageNumber<1 || pageSize<1)
+            throw new DaoException("Invalid pageNumber or pageSize");
+        this.getListValidation(typeId, atributesId, values, operators, atributesIdView);
     }
 
     @Override
     public void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView) {
+        if (!idInTable(TABLE_ENTITYTYPE,typeId))
+            throw new DaoException("Invalid TypeId");
 
     }
 
@@ -45,7 +60,16 @@ public class EntityDaoValidation implements IEntityValidation  {
     public void rowCounterValidation(int typeId, String atributesId, String values, String operators) {
 
     }
+
+    @Override
+    public boolean idInTable(String tableName, int id) {
+        return true;
+    }
+
     private boolean validAttribute (int typeId, int attributeId){
+        return true;
+    }
+    private boolean validValues (int typeId, int attributeId,int entityId){
         return true;
     }
 }
--- src/main/java/com/netcracker/crm/dao/validation/IEntityValidation.java
+++ src/main/java/com/netcracker/crm/dao/validation/IEntityValidation.java
@@ -11,9 +11,11 @@ import java.util.List;
 public interface IEntityValidation {
     void updateValidation(int id, String entityName, int isActive, int userId, List<Value> valuesArr);
     void addValidation(Entity entity) ;
+    void getByIdValidation(int id);
     void updateByEntityValidation(Entity entity);
     void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView,int pageNumber, int pageSize);
     void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView);
     void getByUserAndTypeValidation(Integer userID, Integer entityTypeID, String atributesIdView);
     void rowCounterValidation(int typeId, String atributesId, String values, String operators);
+    boolean idInTable(String tableName, int id);
 }
