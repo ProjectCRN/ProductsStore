71e8b98b886effbefa56217e43b9c96f0b851057
NastyaViter <nastya.bodrova@gmail.com>
2016-12-04 20:02:13 +0300

    Add files via upload

--- src/main/java/com/netcracker/crm/dao/validation/EntityDaoValidation.java
+++ src/main/java/com/netcracker/crm/dao/validation/EntityDaoValidation.java
@@ -1,7 +1,6 @@
 package com.netcracker.crm.dao.validation;
 
 import com.netcracker.crm.dao.IEntityDao;
-
 import static com.netcracker.crm.dao.constants.DaoConstants.*;
 
 import com.netcracker.crm.dao.exception.DaoException;
@@ -15,10 +14,93 @@ import java.util.List;
 /**
  * Created by Nastya on 12/4/2016.
  */
-public class EntityDaoValidation implements IEntityValidation {
-    private final List<String> operators = (Arrays.asList(">", "<", "<=", ">=", "=", "like", "isnull"));
+public class EntityDaoValidation implements IEntityValidation  {
+    private static final List<String> operators = (Arrays.asList(">","<","<=",">=","=","like","isnull"));
+
 
     public static void updateValidation(int id, String entityName, int isActive, int userId, List<Value> valuesArr) {
+       validEntity(id,entityName,isActive,userId,valuesArr);
+    }
+
+
+    public static void addValidation(Entity entity) {
+        validEntity(entity.getId(),entity.getEntityName(), entity.getisActive(),entity.getEntityUserId(),entity.getValueList());
+    }
+
+    public static void getByIdValidation(int id) {
+        if (!idInTable(TABLE_ENTITY,id))
+            throw new DaoException("Invalid Id");
+    }
+
+    public static void updateByEntityValidation(Entity entity) {
+
+    }
+
+    public static void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView, int pageNumber, int pageSize) {
+        if(pageNumber<1 || pageSize<1) {
+            throw new DaoException("Invalid pageNumber or pageSize");
+        }
+        getListValidation(typeId, atributesId, values, operators, atributesIdView);
+    }
+
+    public static void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView) {
+        rowCounterValidation(typeId,atributesId,values,operators);
+        if(!atributesIdView.equals("")) {
+            String[] arr = atributesIdView.split(",");
+            for (String ss : arr) {
+                if (!validAttribute(typeId, Integer.valueOf(ss))) {
+                    throw new DaoException("Invalid attribute for View string");
+                }
+            }
+        }
+    }
+
+    public static void getByUserAndTypeValidation(Integer userID, Integer entityTypeID, String atributesIdView) {
+
+    }
+
+    public static void rowCounterValidation(int typeId, String atributesId, String values, String operators) {
+        String[] arr;
+        int attributeCount=0,valueCount=0,operatorCount=0;
+        if (!idInTable(TABLE_ENTITYTYPE,typeId)){
+            throw new DaoException("Invalid TypeId");
+        }
+        if(!atributesId.equals("")){
+            arr = atributesId.split(",");
+            for ( String ss : arr) {
+                attributeCount++;
+                if(!validAttribute(typeId,Integer.valueOf(ss))){
+                    throw new DaoException("Invalid attribute string");
+                }
+            }
+        }
+        if(!values.equals("")) {
+            arr = values.split(",");
+            for (String ss : arr) {
+                valueCount++;
+                if (!validValues(typeId, Integer.valueOf(ss))) {
+                    throw new DaoException("Invalid value string");
+                }
+            }
+        }
+        if(!operators.equals("")) {
+            arr = operators.split(",");
+            for (String ss : arr) {
+                operatorCount++;
+                if (!operators.contains(ss)) {
+                    throw new DaoException("Invalid operator string");
+                }
+            }
+        }
+        if(attributeCount!=valueCount || attributeCount!=operatorCount){
+            throw new DaoException("Invalid count of attribute, value or operator string");
+        }
+    }
+
+    public static boolean idInTable(String tableName, int id) {
+        return true;
+    }
+    private static void validEntity(int id, String entityName, int isActive, int userId, List<Value> valuesArr){
         if (!idInTable(TABLE_ENTITY, id))
             throw new DaoException("Invalid Id");
         if (entityName == null)
@@ -43,48 +125,10 @@ public class EntityDaoValidation implements IEntityValidation {
         }
     }
 
-    public void addValidation(Entity entity) {
-
-    }
-
-    public void getByIdValidation(int id) {
-
-    }
-
-    public void updateByEntityValidation(Entity entity) {
-
-    }
-
-    public void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView, int pageNumber, int pageSize) {
-        if (pageNumber < 1 || pageSize < 1)
-            throw new DaoException("Invalid pageNumber or pageSize");
-        this.getListValidation(typeId, atributesId, values, operators, atributesIdView);
-    }
-
-    public void getListValidation(int typeId, String atributesId, String values, String operators, String atributesIdView) {
-        if (!idInTable(TABLE_ENTITYTYPE, typeId))
-            throw new DaoException("Invalid TypeId");
-
-    }
-
-    public void getByUserAndTypeValidation(Integer userID, Integer entityTypeID, String atributesIdView) {
-
-    }
-
-    public void rowCounterValidation(int typeId, String atributesId, String values, String operators) {
-
-    }
-
-    public static boolean idInTable(String tableName, int id) {
-
+    private static boolean validAttribute (int typeId, int attributeId){
         return true;
     }
-
-    private boolean validAttribute(int typeId, int attributeId) {
-        return true;
-    }
-
-    private boolean validValues(int typeId, int attributeId, int entityId) {
+    private static boolean validValues (int typeId, int attributeId){
         return true;
     }
 }
