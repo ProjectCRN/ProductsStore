6147b861d8303e473f9aa8afb077a15965aaea87
DiRaskolnikova <diana377577@gmail.com>
2016-12-13 13:47:52 +0300

    fix sql for bat

---
+++ Database/Scripts/RunNotAllwhenexist.bat
@@ -0,0 +1,19 @@
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\00_dropAll.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\02_create_sequance.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\03_create_tables.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\04_create_global_table.sql
+
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\fn_error_message_get.sql
+
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\01_Role.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\02_User.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\03_AtributeType.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\04_EntityType.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\05_Atribute.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\06_Entity.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\07_Value.sql
+
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\sp_entity_list.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\sp_count_rows.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\sp_entity_listValues.sql
+sqlplus -SILENT "ns_admin/qwerty@xe" @AllForBat\sp_entity_NoPaging.sql
